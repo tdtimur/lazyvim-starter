8cd5446c4fa315b1a72c6b5553a44040fc1c2b80
tahta d. timur <tahtadaritimoer@gmail.com>
2024-01-22 22:13:59 +0700

    edit gitsigns position

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -13,7 +13,7 @@ return {
     current_line_blame = true,
     current_line_blame_opts = {
       virt_text = true,
-      virt_text_pos = "eol",
+      virt_text_pos = "right_align",
       delay = 500,
       ignore_whitespace = false,
       virt_text_priority = 100,
