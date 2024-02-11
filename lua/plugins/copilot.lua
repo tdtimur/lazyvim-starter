86ea735987162e52b89e6675fe7721d5f7e8efa3
tahta d. timur <tahtadaritimoer@gmail.com>
2024-02-11 08:20:32 +0700

    added copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,15 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  opts = {
+    suggestion = { enabled = true },
+    panel = {
+      enabled = true
+    },
+    filetypes = {
+      markdown = true,
+      help = true,
+    },
+  },
+}
