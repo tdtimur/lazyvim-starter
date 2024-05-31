843105c6e09b9c5d41caa789ec26fa2d7c0c5d92
tahta d. timur <tahtadaritimoer@gmail.com>
2024-05-31 18:26:17 +0700

    add lualine

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,9 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-vim.api.nvim_create_autocmd('VimEnter', {
+local augroup = vim.api.nvim_create_augroup("VenvSelectorRetrieve", { clear = true })
+vim.api.nvim_create_autocmd({ 'LspAttach' }, {
   desc = 'Auto select virtualenv Nvim open',
-  pattern = '*',
+  pattern = { '*.py' },
+  group = augroup,
   callback = function()
     local venv = vim.fn.findfile('pyproject.toml', vim.fn.getcwd() .. ';')
     if venv ~= '' then
