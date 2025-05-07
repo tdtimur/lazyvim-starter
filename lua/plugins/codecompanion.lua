cc0473087d914e549cfc3126f75ba3844645da65
tahta d. timur <tahtadaritimoer@gmail.com>
2025-05-07 20:45:00 +0700

    added codecomp

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -9,10 +9,33 @@ return {
       adapters = {
         openai = function()
           return require("codecompanion.adapters").extend("openai", {
-            name = "openai-4o-mini",
+            name = "openai",
             schema = {
               model = {
                 default = "gpt-4o-mini",
+              },
+            },
+          })
+        end,
+        gemini = function()
+          return require("codecompanion.adapters").extend("gemini", {
+            name = "gemini",
+            schema = {
+              model = {
+                order = 1,
+                mapping = "parameters",
+                type = "enum",
+                desc =
+                "The model that will complete your prompt. See https://ai.google.dev/gemini-api/docs/models/gemini#model-variations for additional details and options.",
+                default = "gemini-2.5-flash-preview-04-17",
+                choices = {
+                  "gemini-2.5-pro-exp-03-25",
+                  "gemini-2.0-flash",
+                  "gemini-2.0-pro-exp-02-05",
+                  "gemini-1.5-flash",
+                  "gemini-1.5-pro",
+                  "gemini-1.0-pro",
+                },
               },
             },
           })
---
+++ lua/plugins/tex.lua
@@ -0,0 +1,29 @@
+return {
+  -- {
+  --   "nvim-treesitter/nvim-treesitter",
+  --   opts = function(_, opts)
+  --     opts.highlight = opts.highlight or {}
+  --     if type(opts.ensure_installed) == "table" then
+  --       vim.list_extend(opts.ensure_installed, { "bibtex" })
+  --     end
+  --     if type(opts.highlight.disable) == "table" then
+  --       vim.list_extend(opts.highlight.disable, { "latex" })
+  --     else
+  --       opts.highlight.disable = { "latex" }
+  --     end
+  --   end,
+  -- },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   optional = true,
+  --   opts = {
+  --     servers = {
+  --       texlab = {
+  --         keys = {
+  --           { "<Leader>K", "<plug>(vimtex-doc-package)", desc = "Vimtex Docs", silent = true },
+  --         },
+  --       },
+  --     },
+  --   },
+  -- }
+}
